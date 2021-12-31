5ee0ba4fe8249dbb464f4b9a3309a3dd9f70194f
Armin Vakil <arminvakil@gmail.com>
2021-12-31 01:36:54 -0500

    figuring out possible variables with their types

--- Source/Dafny/HoleEvaluator.cs
+++ Source/Dafny/HoleEvaluator.cs
@@ -26,45 +26,160 @@ namespace Microsoft.Dafny {
           // Console.WriteLine($"{c} {topLevelDecl.FullDafnyName}");
           if (topLevelDecl.FullDafnyName == funcName) {
             Console.WriteLine("Found desired function.");
-            ListArguments(topLevelDecl);
+            ListArguments(program, topLevelDecl);
           }
           c++;
         }
       }
     }
 
-    public static void ListArguments(Function func)
+    public static void ListArguments(Program program, Function func)
     {
       foreach (var formal in func.Formals) {
-        Console.WriteLine($"{formal.Name}\t{formal.Type.ToString()}");
+        Console.WriteLine($"\n{formal.Name}\t{formal.Type.ToString()}");
         // Console.WriteLine(formal.Type.NormalizeExpand().IsTopLevelTypeWithMembers);
         var c = 0;
-        Console.WriteLine(formal.DefaultValue);
-        foreach (var expr in TraverseFormal(Expression.CreateIdentExpr(formal)))
+        foreach (var expr in TraverseFormal(program, formal))
         {
-          Console.WriteLine($"{c} {Printer.ExprToString(expr)}:{expr.Type}");
+          Console.WriteLine($"{c} {expr.Name,-20}:\t{expr.Type}");
           c++;
         }
+
       }
     }
 
-    public static IEnumerable<Expression> TraverseFormal(Expression expr)
+    public static IEnumerable<IVariable> TraverseFormal(Program program, IVariable variable)
     {
-      Contract.Requires(expr != null);
-      yield return expr;
-      var members = expr.Type.NormalizeExpand().AsTopLevelTypeWithMembers;
-      foreach(var mem in members.Members)
-      {
-        Console.WriteLine(mem);
+      yield return variable;
+      Contract.Requires(variable != null);
+      var t = variable.Type;
+      if (t is BoolType || t is CharType || t is IntType || t is BigOrdinalType ||
+          t is RealType || t is BitvectorType || t is CollectionType) {
+        // Console.WriteLine("pre-defined variable type");
+        yield break;
       }
+      var udt = (UserDefinedType)t;
+      var cl = udt.ResolvedClass;
+      if (cl is OpaqueTypeDecl) {
+        var otd = (OpaqueTypeDecl)cl;
+        // Console.WriteLine($"{variable.Name} is OpaqueTypeDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is TypeParameter) {
+        var tp = (TypeParameter)cl;
+        // Console.WriteLine($"{variable.Name} is TypeParameter");
+        // TODO traverse underlying definition as well.
+      } else if (cl is InternalTypeSynonymDecl) {
+        var isyn = (InternalTypeSynonymDecl)cl;
+        // Console.WriteLine($"{variable.Name} is InternalTypeSynonymDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is NewtypeDecl) {
+        var td = (NewtypeDecl)cl;
+        // Console.WriteLine($"{variable.Name} is NewtypeDecl");
+        foreach (var v in TraverseType(program, td.BaseType))
+        {
+          var ngv = (Formal)variable;
+          var dotVar = new Formal(ngv.tok, ngv.Name + "." + v.Name, v.Type, true, true, null);
+          // Console.WriteLine($"Constructing dot var:{dotVar.Name}");
+          yield return dotVar;
+        }
+      } else if (cl is SubsetTypeDecl) {
+        var td = (SubsetTypeDecl)cl;
+        // Console.WriteLine($"{variable.Name} is SubsetTypeDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is ClassDecl) {
+        // Console.WriteLine($"{variable.Name} is ClassDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is DatatypeDecl) {
+        var dt = (DatatypeDecl)cl;
+        var subst = Resolver.TypeSubstitutionMap(dt.TypeArgs, udt.TypeArgs);
+        // Console.WriteLine($"{variable.Name} is DatatypeDecl");
+        foreach (var formal in dt.GetGroundingCtor().Formals) {
+          // Console.WriteLine($"type={formal.Type} => {Resolver.SubstType(formal.Type, subst)}");
+          var convertedFormal = new Formal(formal.tok, formal.Name, 
+              Resolver.SubstType(formal.Type, subst), formal.InParam, formal.IsGhost,
+              formal.DefaultValue, formal.IsOld, formal.IsNameOnly, formal.NameForCompilation);
+          foreach (var v in TraverseFormal(program, convertedFormal))
+          {
+            var ngv = (Formal)variable;
+            var dotVar = new Formal(ngv.tok, ngv.Name + "." + v.Name, v.Type, true, true, null);
+            // Console.WriteLine($"Constructing dot var:{dotVar.Name}");
+            yield return dotVar;
+          }
+          // Console.WriteLine($"aaaa {formal.Name}");
+        }
+      }
+      // var members = expr.Type.NormalizeExpand().AsTopLevelTypeWithMembers;
+      // foreach(var mem in members.Members)
+      // {
+      //   Console.WriteLine(mem);
+      // }
       // if (expr.SubExpressions != null)
       // {
-        foreach (var subexpr in expr.SubExpressions)
-        {
-          TraverseFormal(subexpr);
-        }
+        // foreach (var subexpr in expr.SubExpressions)
+        // {
+        //   TraverseFormal(subexpr);
+        // }
       // }
     }
+
+    public static IEnumerable<IVariable> TraverseType(Program program, Type t)
+    {
+      // Console.WriteLine(t.ToString());
+      if (t is BoolType || t is CharType || t is IntType || t is BigOrdinalType ||
+          t is RealType || t is BitvectorType || t is CollectionType) {
+        // Console.WriteLine("pre-defined type");
+        yield break;
+      }
+      var udt = (UserDefinedType)t;
+      var cl = udt.ResolvedClass;
+      if (cl is OpaqueTypeDecl) {
+        var otd = (OpaqueTypeDecl)cl;
+        // Console.WriteLine($"{t.ToString()} is OpaqueTypeDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is TypeParameter) {
+        var tp = (TypeParameter)cl;
+        // Console.WriteLine($"{t.ToString()} is TypeParameter");
+        // TODO traverse underlying definition as well.
+      } else if (cl is InternalTypeSynonymDecl) {
+        var isyn = (InternalTypeSynonymDecl)cl;
+        // Console.WriteLine($"{t.ToString()} is InternalTypeSynonymDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is NewtypeDecl) {
+        var td = (NewtypeDecl)cl;
+        // Console.WriteLine($"{t.ToString()} is NewtypeDecl");
+        foreach (var v in TraverseType(program, td.BaseType))
+        {
+          yield return v;
+        }
+      } else if (cl is SubsetTypeDecl) {
+        var td = (SubsetTypeDecl)cl;
+        // Console.WriteLine($"{t.ToString()} is SubsetTypeDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is ClassDecl) {
+        // Console.WriteLine($"{t.ToString()} is ClassDecl");
+        // TODO traverse underlying definition as well.
+      } else if (cl is DatatypeDecl) {
+        var dt = (DatatypeDecl)cl;
+        // Console.WriteLine($"{t.ToString()} is DatatypeDecl");
+        // TODO traverse underlying definition as well.
+      }
+      else {
+        // Console.WriteLine($"{t.ToString()} is unknown");
+      }
+      // var members = expr.Type.NormalizeExpand().AsTopLevelTypeWithMembers;
+      // foreach(var mem in members.Members)
+      // {
+      //   Console.WriteLine(mem);
+      // }
+      // if (expr.SubExpressions != null)
+      // {
+        // foreach (var subexpr in expr.SubExpressions)
+        // {
+        //   TraverseFormal(subexpr);
+        // }
+      // }
+    }
+
 
   }
 }
