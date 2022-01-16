059cc2102d85191abab602a59965a27d83912ed1
Armin Vakil <arminvakil@gmail.com>
2022-01-16 10:08:53 -0500

    some code refactoring

--- Source/Dafny/HoleEvaluator.cs
+++ Source/Dafny/HoleEvaluator.cs
@@ -16,47 +16,117 @@ namespace Microsoft.Dafny {
 
   public class HoleEvaluator {
 
-    public static void Evaluate(Program program, string funcName)
-    {
+    public static void Evaluate(Program program, string funcName) {
       // Console.WriteLine($"hole evaluation begins for func {funcName}");
       foreach (var kvp in program.ModuleSigs) {
         // iterate over all functions / predicates (this doesn't include lemmas)
         foreach (var topLevelDecl in ModuleDefinition.AllFunctions(kvp.Value.ModuleDef.TopLevelDecls)) {
-          // Console.WriteLine($"{c} {topLevelDecl.FullDafnyName}");
           if (topLevelDecl.FullDafnyName == funcName) {
             Console.WriteLine("Found desired function.");
             var expressions = ListArguments(program, topLevelDecl);
             var c = 0;
+            Dictionary<string, List<Expression>> typeToExpressionDict = new Dictionary<string, List<Expression>>();
             foreach (var expr in expressions) {
               c++;
-              Console.WriteLine($"{c,2} {expr} {expr.tok.val} !!{expr.Type}");
-              if (expr is ExprDotName) {
-                var edt = expr as ExprDotName;
-                Console.WriteLine($"   {edt.Lhs} {edt.Lhs.tok.val}");
+              Console.WriteLine($"{c,2} {Printer.ExprToString(expr),-20} {expr.Type}");
+              if (typeToExpressionDict.ContainsKey(expr.Type.ToString())) {
+                typeToExpressionDict[expr.Type.ToString()].Add(expr);
+              } else {
+                var lst = new List<Expression>();
+                lst.Add(expr);
+                typeToExpressionDict.Add(expr.Type.ToString(), lst);
               }
               // AddExpression(program, topLevelDecl, expr);
+            }
+            Console.WriteLine("");
+            foreach (var k in typeToExpressionDict.Keys) {
+              foreach (var v in typeToExpressionDict[k]) {
+                Console.WriteLine($"{Printer.ExprToString(v),-20} {v.Type}");
+              }
+            }
+            Function topLevelDeclCopy = new Function(
+              topLevelDecl.tok, topLevelDecl.Name, topLevelDecl.HasStaticKeyword,
+              topLevelDecl.IsGhost, topLevelDecl.TypeArgs, topLevelDecl.Formals,
+              topLevelDecl.Result, topLevelDecl.ResultType, topLevelDecl.Req,
+              topLevelDecl.Reads, topLevelDecl.Ens, topLevelDecl.Decreases,
+              topLevelDecl.Body, topLevelDecl.ByMethodTok, topLevelDecl.ByMethodBody,
+              topLevelDecl.Attributes, topLevelDecl.SignatureEllipsis);
+            // check equality of each pair of same type expressions
+            var cnt = 0;
+            using (var wr = new System.IO.StringWriter()) {
+              var pr = new Printer(wr);
+              pr.PrintProgram(program, true);
+              File.WriteAllTextAsync($"/tmp/{funcName}_base.dfy",
+                Printer.ToStringWithoutNewline(wr));
+            }
+            foreach (var k in typeToExpressionDict.Keys) {
+              var values = typeToExpressionDict[k];
+              for (int i = 0; i < values.Count; i++) {
+                for (int j = i + 1; j < values.Count; j++) {
+                  cnt = cnt + 1;
+                  var equalityExpr = Expression.CreateEq(values[i], values[j], values[i].Type);
+                  var expr = equalityExpr;
+                  PrintExpr(program, topLevelDecl, expr, cnt);
+                  topLevelDecl.Body = topLevelDeclCopy.Body;
+                  // var neqExpr = Expression.CreateNot(equalityExpr.tok, equalityExpr);
+                  // var cond = Expression.CreateAnd(topLevelDeclCopy.Body, neqExpr);
+                  // var expr = Expression.CreateITE(cond, Expression.CreateBoolLiteral(equalityExpr.tok, true), equalityExpr);
+                }
+              }
             }
           }
         }
       }
     }
 
-    // public static void AddExpression(Program program, Function func)
+    public static void PrintExpr(Program program, Function func, Expression expr, int cnt) {
+      Console.WriteLine($"{Printer.ExprToString(expr)}");
+
+      var funcName = func.FullDafnyName;
+      string parameterNameTypes = "";
+      string paramNames = "";
+      foreach (var param in func.Formals) {
+        parameterNameTypes += param.Name + ":" + param.Type.ToString() + ", ";
+        paramNames += param.Name + ", ";
+      }
+      parameterNameTypes = parameterNameTypes.Remove(parameterNameTypes.Length - 1, 1);
+      paramNames = paramNames.Remove(paramNames.Length - 2, 2);
+      string lemmaForExprValidityString = "lemma checkReachableStatesNotBeFalse(";
+      lemmaForExprValidityString += parameterNameTypes + ")\n";
+      lemmaForExprValidityString += "  requires ";
+      lemmaForExprValidityString += funcName + "(" + paramNames + ")\n";
+      lemmaForExprValidityString += " ensures false\n{}";
+
+      using (var wr = new System.IO.StringWriter()) {
+        var pr = new Printer(wr);
+        func.Body = Expression.CreateAnd(func.Body, expr);
+        pr.PrintProgram(program, true);
+        File.WriteAllTextAsync($"/tmp/{funcName}_{cnt}.dfy",
+          $"// {Printer.ExprToString(expr)}\n" + Printer.ToStringWithoutNewline(wr) +
+          "\n\n" + lemmaForExprValidityString + "\n");
+        // Console.WriteLine(Printer.ToStringWithoutNewline(wr));
+        // Console.WriteLine("");
+      }
+      // Printer.PrintFunction(transformedFunction, 0, false);
+    }
+
+    // public static Function AddExpression(Function func, Expression expr)
     // {
-    //   foreach(var expr in Expression.Conjuncts(func.Body)) {
-    //     Console.WriteLine($"{expr.tok.val}");
-    //   }
+    //   Function result = new Function(func.tok, func.Name, func.HasStaticKeyword, func.IsGhost,
+    //   func.TypeArgs, func.Formals, func.Result, func.ResultType,
+    //   func.Req, func.Reads, func.Ens, func.Decreases,
+    //   Expression.CreateAnd(func.Body, expr), func.ByMethodTok, func.ByMethodBody,
+    //   func.Attributes, func.SignatureEllipsis);
+    //   return result;
     // }
 
-    public static IEnumerable<Expression> ListArguments(Program program, Function func)
-    {
+    public static IEnumerable<Expression> ListArguments(Program program, Function func) {
       foreach (var formal in func.Formals) {
         // Console.WriteLine($"\n{formal.Name}\t{formal.Type.ToString()}");
         // Console.WriteLine(formal.Type.NormalizeExpand().IsTopLevelTypeWithMembers);
         // var c = 0;
         var identExpr = Expression.CreateIdentExpr(formal);
-        foreach (var expr in TraverseFormal(program, identExpr))
-        {
+        foreach (var expr in TraverseFormal(program, identExpr)) {
           // Console.WriteLine($"{c} {variable.Name,-20}:\t{variable.Type}");
           // c++;
           yield return expr;
@@ -65,8 +135,7 @@ namespace Microsoft.Dafny {
       }
     }
 
-    public static IEnumerable<Expression> TraverseFormal(Program program, Expression expr)
-    {
+    public static IEnumerable<Expression> TraverseFormal(Program program, Expression expr) {
       Contract.Requires(expr != null);
       yield return expr;
       var t = expr.Type;
@@ -92,8 +161,7 @@ namespace Microsoft.Dafny {
       } else if (cl is NewtypeDecl) {
         var td = (NewtypeDecl)cl;
         // Console.WriteLine($"{variable.Name} is NewtypeDecl");
-        foreach (var v in TraverseType(program, td.BaseType))
-        {
+        foreach (var v in TraverseType(program, td.BaseType)) {
           // var ngv = (Formal)variable;
           // var dotVar = new Formal(ngv.tok, ngv.Name + "." + v.Name, v.Type, true, true, null);
           var e = new ExprDotName(v, expr, v.val, null);
@@ -123,8 +191,7 @@ namespace Microsoft.Dafny {
             // var identExpr = Expression.CreateIdentExpr(convertedFormal);
             var exprDot = new ExprDotName(formal.tok, expr, formal.tok.val, null);
             exprDot.Type = Resolver.SubstType(formal.Type, subst);
-            foreach (var v in TraverseFormal(program, exprDot))
-            {
+            foreach (var v in TraverseFormal(program, exprDot)) {
               // Console.WriteLine($"aaa {v.tok.val}");
               // // var ngv = (Formal)variable;
               // // var dotVar = new Formal(ngv.tok, ngv.Name + "." + v.Name, v.Type, true, true, null);
@@ -144,15 +211,14 @@ namespace Microsoft.Dafny {
       // }
       // if (expr.SubExpressions != null)
       // {
-        // foreach (var subexpr in expr.SubExpressions)
-        // {
-        //   TraverseFormal(subexpr);
-        // }
+      // foreach (var subexpr in expr.SubExpressions)
+      // {
+      //   TraverseFormal(subexpr);
+      // }
       // }
     }
 
-    public static IEnumerable<IToken> TraverseType(Program program, Type t)
-    {
+    public static IEnumerable<IToken> TraverseType(Program program, Type t) {
       // Console.WriteLine(t.ToString());
       if (t is BoolType || t is CharType || t is IntType || t is BigOrdinalType ||
           t is RealType || t is BitvectorType || t is CollectionType) {
@@ -176,8 +242,7 @@ namespace Microsoft.Dafny {
       } else if (cl is NewtypeDecl) {
         var td = (NewtypeDecl)cl;
         // Console.WriteLine($"{t.ToString()} is NewtypeDecl");
-        foreach (var v in TraverseType(program, td.BaseType))
-        {
+        foreach (var v in TraverseType(program, td.BaseType)) {
           yield return v;
         }
       } else if (cl is SubsetTypeDecl) {
@@ -191,8 +256,7 @@ namespace Microsoft.Dafny {
         var dt = (DatatypeDecl)cl;
         // Console.WriteLine($"{t.ToString()} is DatatypeDecl");
         // TODO traverse underlying definition as well.
-      }
-      else {
+      } else {
         // Console.WriteLine($"{t.ToString()} is unknown");
       }
       // var members = expr.Type.NormalizeExpand().AsTopLevelTypeWithMembers;
@@ -202,10 +266,10 @@ namespace Microsoft.Dafny {
       // }
       // if (expr.SubExpressions != null)
       // {
-        // foreach (var subexpr in expr.SubExpressions)
-        // {
-        //   TraverseFormal(subexpr);
-        // }
+      // foreach (var subexpr in expr.SubExpressions)
+      // {
+      //   TraverseFormal(subexpr);
+      // }
       // }
     }
 
