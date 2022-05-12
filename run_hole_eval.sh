6bf4235e3b8105388341677489cfc86f45ac2002
Armin Vakil <arminvakil@gmail.com>
2022-05-12 01:09:32 -0400

    change working directory

--- run_hole_eval.sh
+++ run_hole_eval.sh
@@ -3,8 +3,8 @@
 cd /proj/vm-vm-PG0/BASE-DIRECTORY/dafny-holeEval
 for i in $@;
 do
-    workingDir=output_new_holeEval_agents.dfy_${i};
+    workingDir=/mnt/output_new_holeEval_agents.dfy_${i};
     rm -rf ${workingDir};
     mkdir -p ${workingDir};
-    ./Binaries/Dafny /rlimit:100000 /compile:0 /allowGlobals /noNLarith /autoTriggers:1 /verifyAllModules /holeEvalWorkingDir:${workingDir} /holeEvalRunOnce /holeEvalBase:Synod.PaxosNextOneAgent /holeEvalRemoveFileLine:agents.dfy:${i} /holeEvalCons:"Proof_Agreement_Invs.Agreement_Chosen_Inv(PaxosNextOneAgent_c, PaxosNextOneAgent_s) && Proof_Agreement_Invs.Agreement_Chosen_Inv(PaxosNextOneAgent_c, PaxosNextOneAgent_s')" /proj/vm-vm-PG0/BASE-DIRECTORY/paxos_proof/paxos/proof_agreement.dfy &> output_new_holeEval_agents.dfy_${i}/output.txt;
+    ./Binaries/Dafny /rlimit:100000 /compile:0 /allowGlobals /noNLarith /autoTriggers:1 /verifyAllModules /holeEvalWorkingDir:${workingDir} /holeEvalRunOnce /holeEvalBase:Synod.PaxosNextOneAgent /holeEvalRemoveFileLine:agents.dfy:${i} /holeEvalCons:"Proof_Agreement_Invs.Agreement_Chosen_Inv(PaxosNextOneAgent_c, PaxosNextOneAgent_s) && Proof_Agreement_Invs.Agreement_Chosen_Inv(PaxosNextOneAgent_c, PaxosNextOneAgent_s')" /proj/vm-vm-PG0/BASE-DIRECTORY/paxos_proof/paxos/proof_agreement.dfy &> ${workingDir}/output.txt;
 done
